ecf13355e66e1e1cfbaa9edad06c572dcf4c4178
Jerry Anselmi <jerry.anselmi@gmail.com>
2018-12-04 19:53:08 -0300

    add league/fractal dependency and match transformer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(\App\Transformer\MatchTransformer::class, function ($app) {
+            return new \App\Transformer\MatchTransformer();
+        });
+
+        $this->app->alias(\App\Transformer\MatchTransformer::class, 'app.transformer.match');
     }
 }
---
+++ app/Transformer/MatchTransformer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Transformer;
+
+use App\Match;
+
+/**
+ * Tranformer for the match model.
+ */
+class MatchTransformer extends \League\Fractal\TransformerAbstract
+{
+    /**
+     * Transform the model data.
+     *
+     * @param  \App\Match  $match
+     *
+     * @return array
+     */
+    public function transform(Match $match)
+    {
+        return [
+            'id' => $match->id,
+            'name' => 'Match'.$match->id,
+            'next' => $match->next,
+            'winner' => $match->winner,
+            'combination' => $match->combination,
+            'board' => $this->makeBoard($match),
+        ];
+    }
+
+    /**
+     * Make a board using match moves.
+     *
+     * @param  \App\Match  $match
+     * 
+     * @return array
+     */
+    public function makeBoard(Match $match)
+    {
+        $sorted = $match->moves()->get()->sortBy('position');
+        $keys = $sorted->pluck('position')->all();
+        $values = $sorted->pluck('move')->all();
+        return array_replace(
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            array_combine($keys, $values)
+        );
+    }
+}
